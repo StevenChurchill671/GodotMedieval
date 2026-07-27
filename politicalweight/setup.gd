f86159e60d44f6ab0bc906d66c6baf6a1b624730
Churchill <churchillsteve05@gmail.com>
2026-07-28 00:36:12 +0100

    Add files via upload
    slightly working 2d map

--- politicalweight/setup.gd
+++ politicalweight/setup.gd
@@ -4,6 +4,21 @@ extends CanvasLayer
 func printSpecsForConfig():
 	print("config specs")
 
+
+var listOfAreaRects = []
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		for rect in listOfAreaRects:
+			rect.set_position(rect.position + Vector2(1,0))
+	if Input.is_action_pressed("right"):
+		for rect in listOfAreaRects:
+			rect.set_position(rect.position + Vector2(-1,0))
+	if Input.is_action_pressed("down"):
+		for rect in listOfAreaRects:
+			rect.set_position(rect.position + Vector2(0,-1))
+	if Input.is_action_pressed("up"):
+		for rect in listOfAreaRects:
+			rect.set_position(rect.position + Vector2(0,1))
 func _ready():
 	printSpecsForConfig()
 	#var newChildOne = SubViewport.new()
@@ -16,7 +31,7 @@ func _ready():
 	#newChildOne.text = "hey"
 
 func createMap(locations):
-	var translateAmount = Vector2(0.2,0.2)
+	var translateAmount = Vector2(-1,1)
 	print("createMap")
 	for location in locations:
 		var housingAreas =  location.passHousingAreas()
@@ -27,6 +42,7 @@ func createMap(locations):
 				rect.color = Color(0.769, 0.113, 0.51, 1.0)
 			if house.size == 8:
 				rect.color = Color(0.0, 0.357, 0.0, 1.0)
-			rect.set_position(Vector2(house.position.x,house.position.z))
-			rect.set_size(Vector2(1,2))
+			rect.set_position(Vector2(house.position.x,house.position.z)*translateAmount)
+			rect.set_size(Vector2(50,100))
+			listOfAreaRects.append(rect)
 			print("rect")
